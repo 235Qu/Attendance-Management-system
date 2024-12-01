895ba0b51c1826092b8598476fce4f527101107f
235Qu <ajay.std207@gmail.com>
2024-12-01 22:19:07 +0530

    Initial commit

---
+++ Attendance-System-main/pages/A-about.php
@@ -0,0 +1,40 @@
+<?php require PATH_PAGES . "TEMPLATE-top.php"; ?>
+<center>
+<h1 class="mb-0" text-align="center">Student Attendance Management System</h1>
+</center>
+<img class="img-fluid d-block mx-auto mb-4" style="max-height:450px;" src="<?=HOST_ASSETS?>campus2.jpg">
+
+<h5 class="text-danger mb-2">
+  <i class="ico-sm icon-sphere"></i> Document
+</h5>
+<div class="bg-white border p-4 mb-4">
+  <ul class="mb-0">
+    
+    <li><a href="https://github.com/settings/repositories" target="_blank">
+      GitHub
+    </a></li>
+  </ul>
+</div>
+
+<h5 class="text-danger mb-2">
+  <i class="ico-sm icon-hammer"></i>Required Tools
+</h5>
+<div class="bg-white border p-4 mb-4">
+  <ul class="mb-0">
+    <li>PHP</li>
+    <li>Bootstrap</a></li>
+    <li>csv.js</a></li>
+    <li>QRCode.js</a></li>
+    <li>HTML5-QRCode Scanner</a></li>
+  </ul>
+</div>
+
+
+
+<h5 class="text-danger mb-2">
+  <i class="ico-sm icon-smile2"></i> Thanks
+</h5>
+
+
+
+<?php require PATH_PAGES . "TEMPLATE-bottom.php"; ?>
